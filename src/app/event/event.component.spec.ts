2d01e1b886b688b88056c4504a53d0f8f7882195
realofcheams <fireonofficial@gmail.com>
2020-10-23 23:49:06 +0200

    tests fix

--- src/app/event/event.component.spec.ts
+++ src/app/event/event.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EventComponent } from './event.component';
+import {EventUpdateComponent} from './event-update.component';
+import {EventRoutingModule} from './event-routing.module';
+import {EventComponent} from './event.component';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('EventComponent', () => {
   let component: EventComponent;
@@ -8,7 +13,8 @@ describe('EventComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EventComponent ]
+      declarations: [EventComponent, EventUpdateComponent],
+      imports: [CommonModule, EventRoutingModule, HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
   });
