6301284e11fadb9c349122ad6dc47b671804e9a1
Alex Catalan <alexcatmu@gmail.com>
2020-10-30 21:23:23 +0100

    fix tests y e2e

--- src/app/event/event-update.component.spec.ts
+++ src/app/event/event-update.component.spec.ts
@@ -1,4 +1,4 @@
-import { ReactiveFormsModule } from '@angular/forms';
+import {ReactiveFormsModule, ValidationErrors} from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventUpdateComponent } from './event-update.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,6 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../services/event.service';
 import { DatePipe } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
+import {MaterialModule} from '../material/material.module';
 
 describe('Update: Working', () => {
     let eventUpdate: EventUpdateComponent;
@@ -15,12 +16,16 @@ describe('Update: Working', () => {
       } as ActivatedRoute;
     const fakeDatePipe = new DatePipe('en-US');
 
-    
+    let errors: ValidationErrors = {
+      required: undefined,
+      maxlength: undefined
+    };
+
     beforeEach(() => {
 
         // refine the test module by declaring the test component
         TestBed.configureTestingModule({
-            imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule],
+            imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule, MaterialModule],
             declarations: [EventUpdateComponent],
             providers: [ {provide: ActivatedRoute, useValue: fakeActivatedRoute}, {provide: DatePipe, useValue: fakeDatePipe},
                          EventService ],
@@ -28,8 +33,8 @@ describe('Update: Working', () => {
 
         // create component and test fixture
         fixture = TestBed.createComponent(EventUpdateComponent);
-        
-        //get test component from the fixture
+
+        // get test component from the fixture
         eventUpdate = fixture.componentInstance;
     });
 
@@ -39,93 +44,90 @@ describe('Update: Working', () => {
 
     it('form invalid when empty', () => {
         expect(eventUpdate.formEvent.valid).toBeFalsy();
-    })
+    });
 
     it('name field validity', () => {
-        let errors = {};
-        let name = eventUpdate.formEvent.controls['name'];
+        const name = eventUpdate.formEvent.controls.name;
         expect(name.valid).toBeFalsy();
 
         // Name field is required
         errors = name.errors || {};
-        expect(errors['required']).toBeTruthy();
+        expect(errors.required).toBeTruthy();
 
         // Set name to sth
-        name.setValue("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        name.setValue('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
         errors = name.errors || {};
-        expect(errors['required']).toBeFalsy();
+        expect(errors.required).toBeFalsy();
+        expect(errors.maxlength).toBeTruthy();
 
         // Set name to sth correct
-        name.setValue("FCB vs RMD");
-        errors = name.errors || {}
-        expect(errors['required']).toBeFalsy();
-        expect(errors['maxLength']).toBeFalsy();
+        name.setValue('FCB vs RMD');
+        errors = name.errors || {};
+        expect(errors.required).toBeFalsy();
+        expect(errors.maxlength).toBeFalsy();
 
     });
 
     it('street field validity', () => {
-        let errors = {};
-        let street = eventUpdate.formEvent.controls['street'];
+        const street = eventUpdate.formEvent.controls.street;
         expect(street.valid).toBeFalsy();
 
         // Street field is required
         errors = street.errors || {};
-        expect(errors['required']).toBeTruthy();
+        expect(errors.required).toBeTruthy();
 
         // Set street to sth
-        street.setValue("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        street.setValue('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
         errors = street.errors || {};
-        expect(errors['required']).toBeFalsy();
+        expect(errors.required).toBeFalsy();
+        expect(errors.maxlength).toBeTruthy();
 
         // Set street to sth correct
-        street.setValue("Camp Nou, Barcelona");
-        errors = street.errors || {}
-        expect(errors['required']).toBeFalsy();
-        expect(errors['maxLength']).toBeFalsy();   
+        street.setValue('Camp Nou, Barcelona');
+        errors = street.errors || {};
+        expect(errors.required).toBeFalsy();
+        expect(errors.maxLength).toBeFalsy();
     });
 
     it('hourIni field validity', () => {
-        let errors = {};
-        let hourIni = eventUpdate.formEvent.controls['hourIni'];
+        const hourIni = eventUpdate.formEvent.controls.hourIni;
         expect(hourIni.valid).toBeFalsy();
 
         // hourIni field is required
         errors = hourIni.errors || {};
-        expect(errors['required']).toBeTruthy();
+        expect(errors.required).toBeTruthy();
 
         // Set hourIni to sth
-        hourIni.setValue("23:00");
+        hourIni.setValue('23:00');
         errors = hourIni.errors || {};
-        expect(errors['required']).toBeFalsy();  
+        expect(errors.required).toBeFalsy();
     });
 
     it('hourEnd field validity', () => {
-        let errors = {};
-        let hourEnd = eventUpdate.formEvent.controls['hourIni'];
+        const hourEnd = eventUpdate.formEvent.controls.hourEnd;
         expect(hourEnd.valid).toBeFalsy();
 
         // hourEnd field is required
         errors = hourEnd.errors || {};
-        expect(errors['required']).toBeTruthy();
+        expect(errors.required).toBeTruthy();
 
         // Set hourEnd to sth
-        hourEnd.setValue("23:00");
+        hourEnd.setValue('23:00');
         errors = hourEnd.errors || {};
-        expect(errors['required']).toBeFalsy();  
+        expect(errors.required).toBeFalsy();
     });
 
     it('minPrice and maxPrice field validity', () => {
-        let errors = {}
-        let minPrice = eventUpdate.formEvent.controls['minPrice'];
-        let maxPrice = eventUpdate.formEvent.controls['minPrice'];
+        const minPrice = eventUpdate.formEvent.controls.minPrice;
+        const maxPrice = eventUpdate.formEvent.controls.minPrice;
 
         // MinPrice field is required
         errors = minPrice.errors || {};
-        expect(errors['required']).toBeTruthy();
+        expect(errors.required).toBeTruthy();
 
         // MaxPrice field is required
         errors = maxPrice.errors || {};
-        expect(errors['required']).toBeTruthy();
+        expect(errors.required).toBeTruthy();
 
         minPrice.setValue(300);
         maxPrice.setValue(200);
@@ -133,4 +135,4 @@ describe('Update: Working', () => {
     });
 
 
-})
+});
